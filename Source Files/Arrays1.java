a38abfc2e1a9135a1f99419043cfd8cd7d83f26a
lightningbolt275@gmail.com <lightningbolt275@gmail.com>
2017-01-25 15:54:49 +0530

    Latest Files for Java Programming....

---
+++ Source Files/Arrays1.java
@@ -0,0 +1,30 @@
+package abcd;
+
+public class Arrays1 {
+	public static void main(String[] args)
+	{
+		// Array Declaration
+		int[] array1 = new int[4];
+		
+		int[] array2 = new int[]{2,3,4,5};
+		
+		int[] array3 = {3,4,5,6,7,8};
+		
+		int[] array4;
+		 
+		array4 = new int[6];
+		
+		array4[0] = 11;
+		array4[1] = 12;
+		array4[2] = 23;
+		array4[3] = 35;
+		array4[4] = 45;
+		array4[5] = 52;
+		
+		System.out.println("Elements of the array4 are.......");
+		for(int x=0; x<(array4.length); x++)
+		{
+			System.out.println(array4[x]);
+		}
+	}
+}
